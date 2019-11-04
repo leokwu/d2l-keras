566b9240e653953ac34528da79ab1b62b097a211
leok <leok.wu@rock-chips.com>
2019-11-04 18:01:06 +0800

    1.delete useless code Signed-off-by: leok.wu <leok.wu@rock-chips.com>

--- LeNet/lenet-mnist.py
+++ LeNet/lenet-mnist.py
@@ -5,14 +5,6 @@ import keras
 
 from keras import backend as K
 
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
 
 batch_size = 128
 num_classes = 10
